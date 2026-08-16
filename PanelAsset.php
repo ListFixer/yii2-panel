a131a331132dc0c5f40385dbb128bad0d8243040
Michael-Labriola <mike@mikelab.com>
2026-08-16 16:48:57 -0400

    Update PanelAsset.php
    JS did not change.

--- PanelAsset.php
+++ PanelAsset.php
@@ -6,6 +6,6 @@ class PanelAsset extends AssetBundle
 {
 	public $sourcePath = ( __DIR__ . '/dist' );
 	public $css = [ 'css/panel.css?v=2' ];
-	public $js = [ 'js/panel.js?v=2' ];
+	public $js = [ 'js/panel.js' ];
 	public $depends = [ 'yii\bootstrap\BootstrapAsset', 'yii\web\JqueryAsset' ];
 }
