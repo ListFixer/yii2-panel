69023dc5fb58977c9634fc67628c5eb4d182b5ff
Michael-Labriola <mike@mikelab.com>
2026-08-16 16:30:13 -0400

    Update PanelAsset.php
    Version change.

--- PanelAsset.php
+++ PanelAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 class PanelAsset extends AssetBundle
 {
 	public $sourcePath = ( __DIR__ . '/dist' );
-	public $css = [ 'css/panel.css' ];
-	public $js = [ 'js/panel.js' ];
+	public $css = [ 'css/panel.css?v=2' ];
+	public $js = [ 'js/panel.js?v=2' ];
 	public $depends = [ 'yii\bootstrap\BootstrapAsset', 'yii\web\JqueryAsset' ];
 }
